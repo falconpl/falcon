914c0436b156fe64222974a4bb5a43333a77b6fd
Jeremy Cowgar <jeremy@falconpl.org>
2008-01-19 23:44:25 +0000

    Non-working shell of zlib stream
    git-svn-id: svn://falconpl.org/falcon/modules/feathers/trunk@41 127a115f-7314-4089-a6a3-bc3257795827

---
+++ zlib/zlibstream.cpp
@@ -0,0 +1,119 @@
+/*
+  * FALCON - The Falcon Programming Language.
+  * FILE: zlibstream.cpp
+  *
+  * Implementation of ZLibStream.
+  * -------------------------------------------------------------------
+  * Author: Jeremy Cowgar
+  * Begin: Jan 19 2008
+  *
+  * -------------------------------------------------------------------
+  * (C) Copyright 2008: the FALCON developers (see list in AUTHORS file)
+  *
+  * See LICENSE file for licensing details.
+  * In order to use this file in its compiled form, this source or
+  * part of it you have to read, understand and accept the conditions
+  * that are stated in the LICENSE file that comes boundled with this
+  * package.
+  */
+
+/**
+ * \file
+ * Implementation of ZLibStream.
+ */
+
+#include "zlibstream.h"
+
+namespace Falcon {
+
+ZLibStream::ZLibStream( int32 size )
+   : Stream( t_memory )
+{
+}
+
+ZLibStream::ZLibStream( const ZLibStream &zlibstream )
+   : Stream( t_memory )
+{
+}
+
+ZLibStream::~ZLibStream()
+{
+}
+
+bool ZLibStream::errorDescription( String &desciption ) const
+{
+   return false;
+}
+
+bool ZLibStream::close()
+{
+   return false;
+}
+
+int32 ZLibStream::read( void *buffer, int32 size )
+{
+   return -1;
+}
+
+bool ZLibStream::readString( String &target, uint32 size )
+{
+   return false;
+}
+
+int32 ZLibStream::write( const void *buffer, int32 size )
+{
+   return -1;
+}
+
+bool ZLibStream::writeString( const String &source, uint32 begin, uint32 end )
+{
+   return false;
+}
+
+bool ZLibStream::put( uint32 chr )
+{
+   return false;
+}
+
+bool ZLibStream::get( uint32 &chr )
+{
+   return false;
+}
+
+int64 ZLibStream::seek( int64 pos, Stream::e_whence w )
+{
+   return -1;
+}
+
+int64 ZLibStream::tell()
+{
+   return -1;
+}
+
+bool ZLibStream::truncate( int64 pos )
+{
+   return false;
+}
+
+int32 ZLibStream::readAvailable( int32 )
+{
+   return -1;
+}
+
+int32 ZLibStream::writeAvailable( int32 )
+{
+   return -1;
+}
+
+int64 ZLibStream::lastError() const
+ { return (int64) m_lastError; }
+
+UserData *ZLibStream::clone()
+{
+   ZLibStream *zstr = new ZLibStream( *this );
+   return zstr;
+}
+
+}
+
+/* end of zlibstream.cpp */
