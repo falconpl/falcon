3fce921e9759e83ea6ef8ddbb1227f67480b0af5
Giancarlo Niccolai <gc@falconpl.org>
2008-12-20 22:39:14 +0000

    pread now available also on MS-Windows
    git-svn-id: svn://falconpl.org/falcon/modules/feathers/trunk@170 127a115f-7314-4089-a6a3-bc3257795827

--- process/process_sys_win.cpp
+++ process/process_sys_win.cpp
@@ -269,6 +269,201 @@ bool spawn( String **argv, bool overlay, bool background, int *returnValue )
    return (int) true;
 }
 
+
+bool spawn_read( String **argv, bool overlay, bool background, int *returnValue, String *sOut )
+{
+   STARTUPINFOA si;
+	STARTUPINFOW siw;
+   PROCESS_INFORMATION proc;
+   int iPos;
+   DWORD iRet;
+   DWORD iFlags;
+   char fullCommand[2048];
+   wchar_t *fullCommand_w = (wchar_t *)fullCommand;
+   char *filePart;
+	wchar_t *filePart_w;
+	bool wideImplemented;
+	String finalCmd;
+
+   // find the command in the path
+	uint32 bufSize = argv[0]->length() * 4 + 1;
+	wchar_t *fileNameBuf = (wchar_t *) memAlloc( bufSize );
+	argv[0]->toWideString( fileNameBuf, bufSize );
+
+   if ( ! SearchPathW( NULL, fileNameBuf, NULL, 1024, fullCommand_w, &filePart_w ) )
+	{
+		if ( GetLastError() == ERROR_CALL_NOT_IMPLEMENTED )
+      {
+			wideImplemented = false;
+         char *charbuf = (char *) fileNameBuf;
+			if( argv[0]->toCString( charbuf, bufSize ) > 0 )
+         {
+				if ( ! SearchPathA( NULL, charbuf, NULL, 2048, fullCommand, &filePart ) )
+			      finalCmd = charbuf;
+				else
+					finalCmd = fullCommand;
+         }
+      }
+		else {
+			wideImplemented = true;
+			finalCmd = fileNameBuf;
+		}
+	}
+	else
+		finalCmd = fullCommand_w;
+
+	memFree( fileNameBuf );
+
+	// build the complete string
+   iPos = 1;
+   while( argv[ iPos ] != 0 ) {
+		finalCmd.append( ' ' );
+		finalCmd.append( *argv[iPos] );
+      iPos++;
+   }
+
+   HANDLE hRead = INVALID_HANDLE_VALUE;
+   HANDLE hWrite = INVALID_HANDLE_VALUE;
+
+   SECURITY_ATTRIBUTES secatt;
+   secatt.nLength = sizeof( secatt );
+   secatt.lpSecurityDescriptor = NULL;
+   secatt.bInheritHandle = TRUE;
+   
+   if ( !CreatePipe( &hRead, &hWrite, &secatt, 0 ) )
+   {
+      *returnValue = GetLastError();
+      return false;
+   }
+
+	if( wideImplemented )
+	{
+		memset( &siw, 0, sizeof( siw ) );
+		siw.cb = sizeof( siw );
+
+		if( background )  {
+			iFlags = DETACHED_PROCESS;
+			siw.dwFlags = STARTF_USESHOWWINDOW; //| //STARTF_USESTDHANDLES
+			siw.wShowWindow = SW_HIDE;
+		}
+		iFlags = 0;
+
+		siw.hStdOutput = hWrite ;
+
+		if( overlay ) {
+			siw.hStdInput = GetStdHandle( STD_INPUT_HANDLE );
+			siw.hStdError = GetStdHandle( STD_ERROR_HANDLE );
+		}
+
+		siw.dwFlags |= STARTF_USESTDHANDLES;
+		bufSize = finalCmd.length() * 4 + 1;
+		fileNameBuf = (wchar_t *) memAlloc( bufSize );
+		finalCmd.toWideString( fileNameBuf, bufSize );
+		if ( ! CreateProcessW( NULL,
+			fileNameBuf,
+			NULL,
+			NULL,
+			TRUE, //Inerhit handles!
+			iFlags,
+			NULL,
+			NULL,
+			&siw,
+			&proc
+			) )
+		{
+			memFree( fileNameBuf );
+			*returnValue = GetLastError();
+			return false;
+		}
+
+		memFree( fileNameBuf );
+	}
+	else {
+		memset( &si, 0, sizeof( si ) );
+		si.cb = sizeof( si );
+
+		if( background )  {
+			iFlags = DETACHED_PROCESS;
+			si.dwFlags = STARTF_USESHOWWINDOW; //| //STARTF_USESTDHANDLES
+			si.wShowWindow = SW_HIDE;
+		}
+			iFlags = 0;
+
+		if( overlay ) {
+			si.hStdInput = GetStdHandle( STD_INPUT_HANDLE );
+			si.hStdError = GetStdHandle( STD_ERROR_HANDLE );
+		}
+
+		si.dwFlags |= STARTF_USESTDHANDLES;
+		si.hStdOutput = hWrite;
+		
+      bufSize = finalCmd.length() * 4 + 1;
+		char *charbuf = (char *) memAlloc( bufSize );
+		finalCmd.toCString( charbuf, bufSize );
+
+		if ( ! CreateProcessA( NULL,
+			charbuf,
+			NULL,
+			NULL,
+			TRUE,  // INHERIT HANDLES!
+			iFlags,
+			NULL,
+			NULL,
+			&si,
+			&proc
+			) )
+		{
+			memFree( charbuf );
+			*returnValue = GetLastError();
+			return false;
+		}
+
+		memFree( charbuf );
+	}
+
+   // read from the input handle
+#define read_buffer_size 4096
+   char buffer[read_buffer_size];
+   DWORD readin;
+   BOOL peek;
+   bool signaled;
+
+   do
+   {
+      signaled = WaitForSingleObject( proc.hProcess, 10 ) == WAIT_OBJECT_0;
+ 
+      // nothing to read?
+      peek = PeekNamedPipe( hRead, NULL, 0,  NULL, &readin, NULL );
+
+      if ( readin > 0 )
+      {
+         ReadFile( hRead, buffer, readin, &readin, false );
+         if ( readin != 0 )
+         {
+            String temp;
+            temp.adopt( buffer, readin, 0 );
+            sOut->append( temp );
+         }
+      }
+      
+   }
+   while( readin > 0 || ! signaled );
+
+   CloseHandle( hRead );
+   // we have to change our streams with the ones of the process.
+   GetExitCodeProcess( proc.hProcess, &iRet );
+   //memFree( completeCommand );
+   if ( overlay ) {
+      _exit(iRet);
+   }
+
+   CloseHandle( proc.hProcess );
+   CloseHandle( proc.hThread );
+   *returnValue = iRet;
+   return (int) true;
+}
+
+
 const char *shellName()
 {
    char *shname = getenv("ComSpec");
