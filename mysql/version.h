9dd337a2d9b8be30cb70d3155123205d8891d7cd
Giancarlo Niccolai <gc@falconpl.org>
2009-06-21 15:42:42 +0000

    Updated version numbers
    git-svn-id: svn://falconpl.org/falcon/modules/dbi/trunk@135 6551dcfb-e141-0410-b529-ba959add1974

--- mysql/version.h
+++ mysql/version.h
@@ -16,9 +16,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_MAJOR      1
-#define VERSION_MINOR      1
-#define VERSION_REVISION   0
+#define VERSION_MAJOR      0
+#define VERSION_MINOR      9
+#define VERSION_REVISION   8
 
 #endif
 
