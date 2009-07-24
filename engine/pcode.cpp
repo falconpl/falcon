651e80f2f12a04c55b8260618f7e79d4d92a5256
Giancarlo Niccolai <gc@falconpl.org>
2009-07-24 22:17:07 +0000

    ops
    git-svn-id: svn://falconpl.org/falcon/core/trunk@1141 78e5f83a-d9f7-4e86-b854-dad15b5eb37a

---
+++ engine/pcode.cpp
@@ -0,0 +1,212 @@
+/*
+   FALCON - The Falcon Programming Language.
+   FILE: pcode.cpp
+
+   Utilities to manage the Falcon Virtual Machine pseudo code.
+   -------------------------------------------------------------------
+   Author: Giancarlo Niccolai
+   Begin: Fri, 24 Jul 2009 19:42:40 +0200
+
+   -------------------------------------------------------------------
+   (C) Copyright 2009: the FALCON developers (see list in AUTHORS file)
+
+   See LICENSE file for licensing details.
+*/
+
+#include <falcon/pcode.h>
+#include <falcon/common.h>
+
+namespace Falcon {
+
+
+void PCODE::convertEndianity( uint32 paramType, byte* targetArea )
+{
+   switch( paramType )
+   {
+      case P_PARAM_INT32:
+      case P_PARAM_STRID:
+      case P_PARAM_LBIND:
+      case P_PARAM_GLOBID:
+      case P_PARAM_LOCID:
+      case P_PARAM_PARID:
+      case P_PARAM_NTD32:
+         *reinterpret_cast<int32 *>(targetArea) = endianInt32(*reinterpret_cast<int32 *>(targetArea) );
+         break;
+
+      case P_PARAM_INT64:
+      case P_PARAM_NTD64:
+      {
+         uint64 value64 = grabInt64( targetArea );
+
+         // high part - low part
+         *reinterpret_cast<uint32 *>(targetArea) = value64 >> 32;
+         *reinterpret_cast<uint32 *>(targetArea+sizeof(uint32)) = (uint32) value64;
+      }
+      break;
+
+      case P_PARAM_NUM:
+      {
+         union t_unumeric {
+            struct t_integer {
+                 uint32 high;
+                 uint32 low;
+              } integer;
+              numeric number;
+         } unumeric;
+
+         unumeric.number = grabNum( targetArea );
+
+         // high part - low part
+         *reinterpret_cast<uint32 *>(targetArea) = unumeric.integer.high;
+         *reinterpret_cast<uint32 *>(targetArea+sizeof(uint32)) = unumeric.integer.low;
+      }
+      break;
+   }
+
+}
+
+
+uint32 PCODE::advanceParam( uint32 paramType )
+{
+   uint32 offset;
+
+   switch( paramType )
+   {
+
+   case P_PARAM_NIL:
+   case P_PARAM_NOTUSED:
+   case P_PARAM_TRUE:
+   case P_PARAM_FALSE:
+   case P_PARAM_REGA:
+   case P_PARAM_REGB:
+   case P_PARAM_REGS1:
+   case P_PARAM_REGL1:
+   case P_PARAM_REGL2:
+      offset = 0;
+      break;
+
+   case P_PARAM_NUM:
+   case P_PARAM_INT64:
+   case P_PARAM_NTD64:
+      offset = 8;
+      break;
+
+   default:
+      offset = 4;
+   }
+
+   return offset;
+}
+
+
+void PCODE::deendianize( byte* code, uint32 codeSize )
+{
+   uint32 iPos =0;
+   byte opcode;
+   while( iPos < codeSize )
+   {
+      opcode = code[ iPos ];
+
+      // get the options
+      iPos += 4;
+      if( code[ 1 ] != 0 )
+      {
+         convertEndianity( code[ 1 ], code + iPos );
+         iPos += advanceParam( code[1] );
+
+         if( code[ 2 ] != 0 )
+         {
+            convertEndianity( code[2], code + iPos );
+            iPos += advanceParam( code[2] );
+
+            if( code[ 3 ] != 0 )
+            {
+               convertEndianity( code[3], code + iPos );
+               iPos += advanceParam( code[3] );
+            }
+         }
+      }
+
+      // if the operation is a switch, it's handled a bit specially.
+      if ( opcode == P_SWCH && opcode == P_SELE )
+      {
+         // get the switch table (aready de-endianized)
+         uint64 sw_count = *reinterpret_cast<uint64 *>(code - sizeof(int64));
+
+         uint16 sw_int = (int16) (sw_count >> 48);
+         uint16 sw_rng = (int16) (sw_count >> 32);
+         uint16 sw_str = (int16) (sw_count >> 16);
+         uint16 sw_obj = (int16) sw_count;
+
+         // Endianize the nil landing
+         *reinterpret_cast<uint32 *>(code+iPos) = endianInt32( *reinterpret_cast<uint32 *>(code+iPos) );
+         iPos += sizeof( uint32 );
+
+         // endianize the integer table
+         while( sw_int > 0 )
+         {
+            // the int64 value
+            uint64 value64 = grabInt64( code+iPos );
+            // high part - low part
+            *reinterpret_cast<uint32 *>(code+iPos) = value64 >> 32;
+            *reinterpret_cast<uint32 *>(code+iPos+sizeof(uint32)) = (uint32) value64;
+            iPos += sizeof( int64 );
+            // and the landing
+            *reinterpret_cast<int32 *>(code+iPos) = endianInt32( *reinterpret_cast<int32 *>(code+iPos) );
+            iPos += sizeof( int32 );
+
+            --sw_int;
+         }
+
+         // endianize the range table
+         while( sw_rng > 0 )
+         {
+              // the int32 value -- start
+              *reinterpret_cast<int32 *>(code+iPos) = endianInt32( *reinterpret_cast<int32 *>(code+iPos) );
+              iPos += sizeof( int32 );
+              // the int32 value -- end
+              *reinterpret_cast<int32 *>(code+iPos) = endianInt32( *reinterpret_cast<int32 *>(code+iPos) );
+              iPos += sizeof( int32 );
+
+              // and the landing
+              *reinterpret_cast<int32 *>(code+iPos) = endianInt32( *reinterpret_cast<int32 *>(code+iPos) );
+              iPos += sizeof( int32 );
+
+              --sw_rng;
+         }
+
+         // endianize the string table
+         while( sw_str > 0 )
+         {
+              // the int32 string index
+              *reinterpret_cast<int32 *>(code+iPos) = endianInt32( *reinterpret_cast<int32 *>(code+iPos) );
+              iPos += sizeof( int32 );
+
+              // and the landing
+              *reinterpret_cast<int32 *>(code+iPos) = endianInt32( *reinterpret_cast<int32 *>(code+iPos) );
+              iPos += sizeof( int32 );
+
+              --sw_str;
+         }
+
+         // endianize the object table
+         while( sw_obj > 0 )
+         {
+              // the int32 symbol index
+              *reinterpret_cast<int32 *>(code+iPos) = endianInt32( *reinterpret_cast<int32 *>(code+iPos) );
+              iPos += sizeof( int32 );
+
+              // and the landing
+              *reinterpret_cast<int32 *>(code+iPos) = endianInt32( *reinterpret_cast<int32 *>(code+iPos) );
+              iPos += sizeof( int32 );
+
+              --sw_obj;
+         }
+      }
+
+   }
+}
+
+}
+
+/* end of pcode.cpp */
