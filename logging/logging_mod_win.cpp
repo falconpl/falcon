6187a6d1297bdad35fe128899212f6f7056509a6
Giancarlo Niccolai <gc@falconpl.org>
2009-09-09 07:25:29 +0000

    Added the correct file for Windows Event Logger support.
    git-svn-id: svn://falconpl.org/falcon/modules/feathers/trunk@276 127a115f-7314-4089-a6a3-bc3257795827

---
+++ logging/logging_mod_win.cpp
@@ -0,0 +1,101 @@
+/*
+   FALCON - The Falcon Programming Language
+   FILE: logging_mod_win.cpp
+
+   Logging module -- module service classes (MS-Windows specific)
+   -------------------------------------------------------------------
+   Author: Giancarlo Niccolai
+   Begin: Sat, 05 Sep 2009 17:21:00 +0200
+
+   -------------------------------------------------------------------
+   (C) Copyright 2009: the FALCON developers (see list in AUTHORS file)
+
+   See LICENSE file for licensing details.
+*/
+
+#include "logging_mod.h"
+#include <falcon/error.h>
+#include <falcon/autowstring.h>
+
+namespace Falcon {
+
+LogChannelSyslog::LogChannelSyslog( const String& identity, uint32 facility, int level ):
+   LogChannel( level ),
+   m_identity( identity ),
+   m_facility( facility )
+{
+   init();
+}
+
+LogChannelSyslog::LogChannelSyslog( const String& identity, const String &fmt, uint32 facility, int level ):
+   LogChannel( fmt, level ),
+   m_identity( identity ),
+   m_facility( facility )
+
+{
+	init();
+}
+
+
+LogChannelSyslog::~LogChannelSyslog()
+{
+   CloseEventLog( (HANDLE) m_sysdata );
+}
+
+void LogChannelSyslog::init()
+{
+   AutoWString appname( m_identity );
+   m_sysdata = (void*) OpenEventLogW( NULL, appname.w_str() );
+   
+   if ( m_sysdata == 0 )
+   {
+      throw new IoError( ErrorParam( FALCON_LOGGING_ERROR_OPEN, __LINE__ )
+		  .origin( e_orig_runtime )
+		  .sysError( GetLastError() ) );
+   }
+}
+
+
+void LogChannelSyslog::writeLogEntry( const String& entry, LogChannel::LogMessage* pOrigMsg )
+{
+   WORD wType;
+   DWORD dwEventID;
+
+   if ( pOrigMsg->m_level <= LOGLEVEL_ERROR )
+   {  
+      wType = EVENTLOG_ERROR_TYPE;
+	  dwEventID = 3 << 30;
+   }
+   else if ( pOrigMsg->m_level == LOGLEVEL_WARN )
+   {
+      wType = EVENTLOG_WARNING_TYPE;
+	  dwEventID = 2 << 30;
+   }
+   else {
+      wType = EVENTLOG_INFORMATION_TYPE;
+	  dwEventID = 1 << 30;
+   }
+
+   // From MS docs; event ID  = gravity | custom | facility | code;
+   dwEventID |= (1 << 29) | ((m_facility&0x1FF) << 16) | (pOrigMsg->m_code & 0xFFFF);
+
+   AutoWString w_msg( entry );
+   
+   const wchar_t* strings[] = { w_msg.w_str() };
+   ReportEventW(
+	   (HANDLE)m_sysdata,	// __in  HANDLE hEventLog,
+	   wType,				// __in  WORD wType,
+	   m_facility,			// __in  WORD wCategory,
+	   dwEventID,			// __in  DWORD dwEventID,
+	   NULL,				// __in  PSID lpUserSid,
+	   1,					// __in  WORD wNumStrings,
+	   0,					// __in  DWORD dwDataSize,
+	   strings,				// __in  LPCTSTR *lpStrings,
+	   NULL					// __in  LPVOID lpRawData
+	   );
+
+}
+	   
+}
+
+/* end of logging_mod_win.cpp */
