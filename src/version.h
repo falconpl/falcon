57e6fd1dff4e2eac214b01f5664e99fd231c4c1e
Giancarlo Niccolai <gc@falconpl.org>
2009-10-10 15:40:37 +0000

    Fixed version file
    git-svn-id: svn://falconpl.org/falcon/modules/dbus/trunk@8 8fef69b5-7b5e-0410-a78b-b4e948c5a9da

--- src/version.h
+++ src/version.h
@@ -1,24 +1,22 @@
 /*
-   @MAIN_PRJ@
+   DBus module for Falcon
    FILE: version.h
 
-   @DESCRIPTION@
-   Interface extension functions
    -------------------------------------------------------------------
-   Author: @AUTHOR@
-   Begin: @DATE@
+   Author: Giancarlo Niccolai
+   Begin: Sat, 10 Oct 2009 17:39:10 +0200
 
    -------------------------------------------------------------------
-   (C) Copyright @YEAR@: @COPYRIGHT@
+   (C) Copyright 2009: (C) Giancarlo Niccolai
 
-   @LICENSE@
+   Licensed under the terms in the copyright file.
 */
 
 #ifndef VERSION_H
 #define VERSION_H
 
 #define VERSION_MAJOR        0
-#define VERSION_MINOR        0 
+#define VERSION_MINOR        1
 #define VERSION_REVISION     0
 
 #endif
