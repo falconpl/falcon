276740af0c67deaf72663c5ba6161ee089813f0e
Steven Oliver <oliver.steven@gmail.com>
2009-12-10 00:33:50 +0000

    Implement most of the Transaction and RecordSet class for Oracle
    git-svn-id: svn://falconpl.org/falcon/modules/dbi/trunk@147 6551dcfb-e141-0410-b529-ba959add1974

--- oracle/oracle_mod.h
+++ oracle/oracle_mod.h
@@ -21,6 +21,67 @@
 
 namespace Falcon
 {
+    class DBIRecordsetOracle : public DBIRecordset
+    {
+        protected:
+            int o_row;
+            int o_rowCount;
+            int o_columnCount;
+
+            // FIXME
+            MYSQL_RES *m_res;
+            MYSQL_FIELD *m_fields;
+            MYSQL_ROW m_rowData;
+            unsigned long *m_fieldLengths;
+
+            static dbi_type getFalconType( int typ );
+
+        public:
+            // FIXME
+            DBIRecordsetMySQL( DBIHandle *dbh, MYSQL_RES *res );
+            ~DBIRecordsetOracle();
+
+            virtual dbi_status next();
+            virtual int getRowCount();
+            virtual int getRowIndex();
+            virtual int getColumnCount();
+            virtual dbi_status getColumnNames( char *names[] );
+            virtual dbi_status getColumnTypes( dbi_type *types );
+            virtual dbi_status asString( const int columnIndex, String &value );
+            virtual dbi_status asBoolean( const int columnIndex, bool &value );
+            virtual dbi_status asInteger( const int columnIndex, int32 &value );
+            virtual dbi_status asInteger64( const int columnIndex, int64 &value );
+            virtual dbi_status asNumeric( const int columnIndex, numeric &value );
+            virtual dbi_status asDate( const int columnIndex, TimeStamp &value );
+            virtual dbi_status asTime( const int columnIndex, TimeStamp &value );
+            virtual dbi_status asDateTime( const int columnIndex, TimeStamp &value );
+            virtual dbi_status asBlobID( const int columnIndex, String &value );
+            virtual void close();
+            virtual dbi_status getLastError( String &description );
+    };
+
+
+    class DBITransactionOracle : public DBITransaction
+    {
+        protected:
+            bool o_inTransaction;
+
+        public:
+            DBITransactionOracle( DBIHandle *dbh );
+
+            virtual DBIRecordset *query( const String &query, int64 &affected_rows, dbi_status &retval );
+            virtual dbi_status begin();
+            virtual dbi_status commit();
+            virtual dbi_status rollback();
+            virtual dbi_status close();
+            virtual dbi_status getLastError( String &description );
+
+            virtual DBIBlobStream *openBlob( const String &blobId, dbi_status &status );
+            virtual DBIBlobStream *createBlob( dbi_status &status, const String &params= "",
+                    bool bBinary = false );
+    };
+
+
     class DBIHandleOracle : public DBIHandle
     {
         protected:
@@ -34,7 +95,7 @@ namespace Falcon
             DBIHandleOracle( ORALCE *conn );
             virtual ~DBIHandleOracle();
 
-            Connection *getConn() { return m_conn; }
+            Connection *getConn() { return o_conn; }
 
             virtual DBITransaction *startTransaction();
             virtual dbi_status closeTransaction( DBITransaction *tr );
