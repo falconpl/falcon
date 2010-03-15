1c4cabfc795b447605bd1c6a45c1d32395110570
Stanislas Marquis <stnsls@gmail.com>
2010-03-15 12:07:26 +0100

    Added a generic property setter to the GObject.

--- src/g_Object.cpp
+++ src/g_Object.cpp
@@ -20,6 +20,10 @@ void Object::modInit( Falcon::Module* mod )
     Gtk::MethodTab methods[] =
     {
     { "signal_notify",      &Object::signal_notify },
+    { "set",                &Object::set },
+    { "notify",             &Object::notify },
+    { "freeze_notify",      &Object::freeze_notify },
+    { "thaw_notify",        &Object::thaw_notify },
     { NULL, NULL }
     };
 
@@ -60,6 +64,121 @@ FALCON_FUNC Object::signal_notify( VMARG )
 void Object::on_notify( GObject* obj, GParamSpec* pspec, gpointer _vm )
 {
     Gtk::internal_trigger_slot( obj, "notify", "on_notify", (VMachine*)_vm );
+}
+
+
+/*#
+    @method set gtk.GObject
+    @brief Sets a property on an object.
+    @param property_name name of the property to set
+    @param value value for the property (nil, integer, boolean, numeric, or string)
+ */
+FALCON_FUNC Object::set( VMARG )
+{
+    Item* i_nam = vm->param( 0 );
+    Item* i_val = vm->param( 1 );
+#ifndef NO_PARAMETER_CHECK
+    if ( !i_nam || i_nam->isNil() || !i_nam->isString()
+        || !i_val )
+        throw_inv_params( "S,?" );
+#endif
+    AutoCString name( i_nam->asString() );
+    MYSELF;
+    GET_OBJ( self );
+
+    if ( i_val->isNil() )
+        g_object_set( _obj, name.c_str(), NULL, NULL );
+    else
+    if ( i_val->isInteger() )
+        g_object_set( _obj, name.c_str(), i_val->asInteger(), NULL );
+    else
+    if ( i_val->isBoolean() )
+        g_object_set( _obj, name.c_str(), i_val->asBoolean(), NULL );
+    else
+    if ( i_val->isNumeric() )
+        g_object_set( _obj, name.c_str(), i_val->asNumeric(), NULL );
+    else
+    if ( i_val->isString() )
+    {
+        AutoCString val( i_val->asString() );
+        g_object_set( _obj, name.c_str(), val.c_str(), NULL );
+    }
+#if 0
+    else
+    if ( i_val->isObject() )
+    {
+        void* obj = i_val->asObject()->getUserData();
+#ifndef NO_PARAMETER_CHECK
+        if ( !obj )
+            throw_inv_params( "No object.." );
+#endif
+        g_object_set( _obj, name.c_str(), obj, NULL );
+    }
+#endif
+    else
+        throw_inv_params( "S,?" );
+}
+
+
+/*#
+    @method notify gtk.GObject
+    @brief Emits a "notify" signal for the property property_name on object.
+    @param property_name (string)
+ */
+FALCON_FUNC Object::notify( VMARG )
+{
+    Item* i_nam = vm->param( 0 );
+#ifndef NO_PARAMETER_CHECK
+    if ( !i_nam || i_nam->isNil() || !i_nam->isString() )
+        throw_inv_params( "S" );
+#endif
+    AutoCString name( i_nam->asString() );
+    MYSELF;
+    GET_OBJ( self );
+    g_object_notify( _obj, name.c_str() );
+}
+
+
+/*#
+    @method freeze_notify gtk.GObject
+    @brief Increases the freeze count on object.
+
+    If the freeze count is non-zero, the emission of "notify" signals on object
+    is stopped. The signals are queued until the freeze count is decreased to zero.
+
+    This is necessary for accessors that modify multiple properties to prevent
+    premature notification while the object is still being modified.
+ */
+FALCON_FUNC Object::freeze_notify( VMARG )
+{
+#ifdef STRICT_PARAMETER_CHECK
+    if ( vm->paramCount() )
+        throw_require_no_args();
+#endif
+    MYSELF;
+    GET_OBJ( self );
+    g_object_freeze_notify( _obj );
+}
+
+
+/*#
+    @method thaw_notify gtk.GObject
+    @brief Reverts the effect of a previous call to freeze_notify().
+
+    The freeze count is decreased on object and when it reaches zero, all queued
+    "notify" signals are emitted.
+
+    It is an error to call this function when the freeze count is zero.
+ */
+FALCON_FUNC Object::thaw_notify( VMARG )
+{
+#ifdef STRICT_PARAMETER_CHECK
+    if ( vm->paramCount() )
+        throw_require_no_args();
+#endif
+    MYSELF;
+    GET_OBJ( self );
+    g_object_thaw_notify( _obj );
 }
 
 
--- src/g_Object.hpp
+++ src/g_Object.hpp
@@ -18,6 +18,14 @@ FALCON_FUNC signal_notify( VMARG );
 
 void on_notify( GObject*, GParamSpec*, gpointer );
 
+FALCON_FUNC set( VMARG );
+
+FALCON_FUNC notify( VMARG );
+
+FALCON_FUNC freeze_notify( VMARG );
+
+FALCON_FUNC thaw_notify( VMARG );
+
 
 } // Object
 } // Glib
