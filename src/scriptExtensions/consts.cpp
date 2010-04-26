9b6b024fda6b2ef62bad208bbe3e2a0d803f5600
Maik Beckmann <beckmann.maik@googlemail.com>
2010-04-26 12:52:36 +0000

    factored out enums and constants into seperate files.
    git-svn-id: svn://falconpl.org/falcon/modules/pdf/trunk@27 ccbdac88-e742-0410-b308-ec639118481f

---
+++ src/scriptExtensions/consts.cpp
@@ -0,0 +1,24 @@
+/*
+ * pdf.cpp
+ *
+ *  Created on: 04.04.2010
+ *      Author: maik
+ */
+
+#include <falcon/engine.h>
+#include <hpdf.h>
+#include "consts.h"
+
+namespace Falcon { namespace Ext { namespace hpdf {
+
+void registerConsts(Falcon::Module* self)
+{
+  using Falcon::int64;
+  self->addConstant("COMP_NONE", (int64) HPDF_COMP_NONE);
+  self->addConstant("COMP_TEXT", (int64) HPDF_COMP_TEXT);
+  self->addConstant("COMP_IMAGE", (int64) HPDF_COMP_IMAGE);
+  self->addConstant("COMP_METADATA", (int64) HPDF_COMP_METADATA);
+  self->addConstant("COMP_ALL", (int64) HPDF_COMP_ALL);
+}
+
+}}} // Falcon::Ext::hpdf
---
+++ src/scriptExtensions/enums.h
@@ -0,0 +1,17 @@
+/*
+ * hpdf_page.h
+ *
+ *  Created on: 03.04.2010
+ *      Author: maik
+ */
+
+#ifndef FALCON_MODULE_EXT_ENUMS_H
+#define FALCON_MODULE_EXT_ENUMS_H
+
+namespace Falcon { namespace Ext { namespace hpdf {
+
+void registerEnums(Falcon::Module*);
+
+}}} // Falcon::Ext::hpdf
+
+#endif // FALCON_MODULE_EXT_ENUMS_H
