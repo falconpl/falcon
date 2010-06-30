a56d6074dbeeee1d596929149f89b5b72d5c38a5
Stanislas Marquis <stnsls@gmail.com>
2010-06-30 12:36:14 +0200

    Added class GtkCellRendererText.

---
+++ src/gtk_CellRendererText.hpp
@@ -0,0 +1,42 @@
+#ifndef GTK_CELLRENDERERTEXT_HPP
+#define GTK_CELLRENDERERTEXT_HPP
+
+#include "modgtk.hpp"
+
+#define GET_CELLRENDERERTEXT( item ) \
+        ((GtkCellRendererText*) Falcon::dyncast<Gtk::CellRendererText*>( (item).asObjectSafe() )->getGObject())
+
+
+namespace Falcon {
+namespace Gtk {
+
+/**
+ *  \class Falcon::Gtk::CellRendererText
+ */
+class CellRendererText
+    :
+    public Gtk::CoreGObject
+{
+public:
+
+    CellRendererText( const Falcon::CoreClass*, const GtkCellRendererText* = 0 );
+
+    static Falcon::CoreObject* factory( const Falcon::CoreClass*, void*, bool );
+
+    static void modInit( Falcon::Module* );
+
+    static FALCON_FUNC init( VMARG );
+
+    static FALCON_FUNC signal_edited( VMARG );
+
+    static void on_edited( GtkCellRendererText*, gchar*, gchar*, gpointer );
+
+    static FALCON_FUNC set_fixed_height_from_font( VMARG );
+
+};
+
+
+} // Gtk
+} // Falcon
+
+#endif // !GTK_CELLRENDERERTEXT_HPP
