727825a6e2db667f835d77410892095d566e0984
Lucone <uomonero67@gmail.com>
2010-10-10 10:17:42 +0200

    correct Sqlite include variable into DBI module

--- modules/native/dbi/cmake/FindSqlite.cmake
+++ modules/native/dbi/cmake/FindSqlite.cmake
@@ -1,8 +1,8 @@
-find_path(Sqlite_INCLUE_DIR sqlite3.h)
+find_path(Sqlite_INCLUDE_DIR sqlite3.h)
 find_library(Sqlite_LIBRARY sqlite3)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Sqlite DEFAULT_MSG Sqlite_LIBRARY Sqlite_INCLUE_DIR)
+find_package_handle_standard_args(Sqlite DEFAULT_MSG Sqlite_LIBRARY Sqlite_INCLUDE_DIR)
 
-set(Sqlite_INCLUDE_DIRS ${Sqlite_INCLUE_DIR})
+set(Sqlite_INCLUDE_DIRS ${Sqlite_INCLUDE_DIR})
 set(Sqlite_LIBRARIES ${Sqlite_LIBRARY})
