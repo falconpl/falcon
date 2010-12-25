8a0016af975fba91add65fe08e76b844ff74c348
Steven Oliver <oliver.steven@gmail.com>
2010-12-24 22:24:33 -0500

    [Oracle] Reformatting

--- modules/native/dbi/oracle/oracle_ext.cpp
+++ modules/native/dbi/oracle/oracle_ext.cpp
@@ -18,59 +18,59 @@
 #include "oracle_ext.h"
 
 /*#
-   @beginmodule oracle
-*/
+  @beginmodule oracle
+  */
 namespace Falcon
 {
-namespace Ext
-{
+    namespace Ext
+    {
 
-/*#
-      @class Oracle
-      @brief Direct interface to Oracle database.
-      @param connect String containing connection parameters.
+        /*#
+          @class Oracle
+          @brief Direct interface to Oracle database.
+          @param connect String containing connection parameters.
 
-      The connect string uses the standard connection values:
-      - username
-      - password
-      - database
+          The connect string uses the standard connection values:
+          - username
+          - password
+          - database
 
-      Oracle does not use ports in its connection string. That information
-      is generally stored in your TNSNAMES.ora file.
-*/
+          Oracle does not use ports in its connection string. That information
+          is generally stored in your TNSNAMES.ora file.
+          */
 
-FALCON_FUNC Oracle_init( VMachine *vm )
-{
-   Item *paramsI = vm->param(0);
-   Item *i_tropts = vm->param(1);
-   if ( ! paramsI || ! paramsI->isString() || ( i_tropts && ! i_tropts->isString() ) )
-   {
-      throw new ParamError( ErrorParam( e_inv_params, __LINE__ )
-                          .extra( "S,[S]" ) );
-   }
+        FALCON_FUNC Oracle_init( VMachine *vm )
+        {
+            Item *paramsI = vm->param(0);
+            Item *i_tropts = vm->param(1);
+            if ( ! paramsI || ! paramsI->isString() || ( i_tropts && ! i_tropts->isString() ) )
+            {
+                throw new ParamError( ErrorParam( e_inv_params, __LINE__ )
+                        .extra( "S,[S]" ) );
+            }
 
-   String *params = paramsI->asString();
+            String *params = paramsI->asString();
 
-   DBIHandle *hand = 0;
-   try
-   {
-       hand = theOracleService.connect( *params );
-       if( i_tropts != 0 )
-       {
-           hand->options( *i_tropts->asString() );
-       }
+            DBIHandle *hand = 0;
+            try
+            {
+                //hand = theOracleService.connect( *params );
+                if( i_tropts != 0 )
+                {
+                    //  hand->options( *i_tropts->asString() );
+                }
 
-       CoreObject *instance = theOracleService.makeInstance( vm, hand );
-       vm->retval( instance );
-   }
-   catch (...)
-   {
-       delete hand;
-       throw ;
-   }
-}
+                //CoreObject *instance = theOracleService.makeInstance( vm, hand );
+                //vm->retval( instance );
+            }
+            catch (...)
+            {
+                //delete hand;
+                throw ;
+            }
+        }
 
-} /* namespace Ext */
+    } /* namespace Ext */
 } /* namespace Falcon */
 
 /* end of oracle_ext.cpp */
