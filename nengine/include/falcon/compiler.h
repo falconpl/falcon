3236dde74f7515749e15c3fda50b44386770ff20
Giancarlo Niccolai <gc@falconpl.org>
2011-02-20 18:04:01 +0100

    Started studying compiler and properties

---
+++ nengine/include/falcon/compiler.h
@@ -0,0 +1,39 @@
+/*
+   FALCON - The Falcon Programming Language.
+   FILE: compiler.h
+
+   Falcon source compiler
+   -------------------------------------------------------------------
+   Author: Giancarlo Niccolai
+   Begin: Sun, 20 Feb 2011 16:41:23 +0100
+
+   -------------------------------------------------------------------
+   (C) Copyright 2011: the FALCON developers (see list in AUTHORS file)
+
+   See LICENSE file for licensing details.
+*/
+
+
+#ifndef FALCON_COMPILER_H
+#define FALCON_COMPILER_H
+
+#include <falcon/vm.h>
+
+namespace Falcon {
+
+class Stream;
+
+class Compiler
+{
+public:
+   Compiler();
+   virtual ~Compiler();
+
+private:
+   VMachine m_vm;
+
+};
+
+}
+
+#endif
