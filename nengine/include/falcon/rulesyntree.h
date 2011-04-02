40dd84b2183169647368ff8f49be7cf1fef6e438
Giancarlo Niccolai <gc@falconpl.org>
2011-04-02 17:49:59 +0200

    Added rules support (missing shortcut/success; the rest is complete)

---
+++ nengine/include/falcon/rulesyntree.h
@@ -0,0 +1,45 @@
+/*
+   FALCON - The Falcon Programming Language.
+   FILE: rulesyntree.h
+
+   Syntactic tree item definitions.
+   -------------------------------------------------------------------
+   Author: Giancarlo Niccolai
+   Begin: Sun, 02 Jan 2011 20:37:39 +0100
+
+   -------------------------------------------------------------------
+   (C) Copyright 2011: the FALCON developers (see list in AUTHORS file)
+
+   See LICENSE file for licensing details.
+*/
+
+#ifndef FALCON_RULESYNTREE_H
+#define FALCON_RULESYNTREE_H
+
+#include <falcon/syntree.h>
+
+namespace Falcon
+{
+
+class Statement;
+
+/** Syntactic tree for rules.
+ *
+ * Evaluates a single list of rules in a rule context.
+ */
+class FALCON_DYN_CLASS RuleSynTree: public SynTree
+{
+
+public:
+   RuleSynTree();
+   virtual ~RuleSynTree();
+
+   static void apply_( const PStep* ps, VMachine* vm );
+};
+
+}
+
+#endif
+
+/* end of rulesyntree.h */
+
---
+++ nengine/tests/ruletest.cpp
@@ -0,0 +1,182 @@
+/*
+ * ruletest.cpp
+ *
+ *  Created on: 15/gen/2011
+ *      Author: gian
+ */
+
+#include <cstdio>
+#include <iostream>
+
+#include <falcon/vm.h>
+#include <falcon/syntree.h>
+#include <falcon/localsymbol.h>
+#include <falcon/error.h>
+#include <falcon/expression.h>
+#include <falcon/exprvalue.h>
+#include <falcon/exprsym.h>
+#include <falcon/statement.h>
+#include <falcon/stmtrule.h>
+#include <falcon/rulesyntree.h>
+#include <falcon/synfunc.h>
+#include <falcon/extfunc.h>
+
+#include <falcon/trace.h>
+#include <falcon/application.h>
+
+using namespace Falcon;
+
+class FuncPrintl: public Function
+{
+public:
+   class NextStep: public PStep
+   {
+   public:
+      NextStep()
+      {
+         apply = apply_;
+      }
+
+      static void apply_( const PStep* ps, VMachine* vm )
+      {
+         const NextStep* nstep = static_cast<const NextStep*>(ps);
+         std::cout << *vm->regA().asString()->c_ize();
+         VMContext* ctx = vm->currentContext();
+         nstep->printNext( vm, ctx->currentCode().m_seqId );
+      }
+
+      void printNext( VMachine* vm, int count ) const
+      {
+         VMContext* ctx = vm->currentContext();
+         int nParams = ctx->currentFrame().m_paramCount;
+
+         while( count < nParams )
+         {
+            Item temp;
+            Class* cls;
+            void* data;
+
+            ctx->param(count)->forceClassInst( cls, data );
+            ++count;
+
+            vm->ifDeep(this);
+            cls->op_toString( vm, data, temp );
+            if( vm->wentDeep() )
+            {
+               ctx->currentCode().m_seqId = count;
+               return;
+            }
+            std::cout << temp.asString()->c_ize();
+         }
+
+         std::cout << std::endl;
+         // we're out of the function.
+         vm->returnFrame();
+      }
+   } m_nextStep;
+
+   FuncPrintl():
+      Function("printl")
+   {}
+
+   virtual ~FuncPrintl() {}
+
+   virtual void apply( VMachine* vm, int32 nParams )
+   {
+      m_nextStep.printNext( vm, 0 );
+   }
+} printl;
+
+
+
+class RuleApp: public Falcon::Application
+{
+
+public:
+   void guardAndGo()
+   {
+      try {
+         go();
+      }
+      catch( Error* e )
+      {
+         std::cout << "Caught: " << e->describe().c_ize() << std::endl;
+         e->decref();
+      }
+   }
+
+void go()
+{
+   // create a program:
+   // rule
+   //   a = 0
+   //   ? a = a + 1
+   //   printl( a )
+   //   a >= 10
+   // end
+   // printl( "A was ", a )
+   //
+
+
+   // the main
+   SynFunc fmain( "__main__" );
+   Falcon::Symbol* var_a = fmain.addVariable("a");
+
+   StmtAutoexpr* assign_expr = new StmtAutoexpr(
+               new ExprAssign( var_a->makeExpression(),
+                  new ExprPlus( var_a->makeExpression(), new ExprValue(1) ) ) );
+   assign_expr->nd(true);
+   // and the rule
+   StmtRule* rule = new StmtRule;
+   (*rule)
+         .addStatement( new StmtAutoexpr(
+               new ExprAssign( var_a->makeExpression(), new ExprValue(0) ) ) )
+         .addStatement( assign_expr )
+         .addStatement( new StmtAutoexpr(&(new ExprCall( new ExprValue(&printl) ))
+            ->addParameter(new ExprValue("A: ")).addParameter(var_a->makeExpression())) )
+         .addStatement( new StmtAutoexpr(
+               new ExprGE( var_a->makeExpression(), new ExprValue(10) ) ) );
+
+   fmain.syntree()
+      .append( rule )
+      .append( new StmtAutoexpr(&(new ExprCall( new ExprValue(&printl) ))
+            ->addParameter(new ExprValue("A was ")).addParameter(var_a->makeExpression())) );
+      
+
+   std::cout << "Will run: "<< std::endl << fmain.syntree().describe().c_ize() << std::endl;
+
+   // And now, run the code.
+   Falcon::VMachine vm;
+   vm.call(&fmain,0);
+   vm.run();
+
+
+   /*
+   while( ! vm.codeEmpty() )
+   {
+      String report;
+      vm.report( report );
+      report.c_ize();
+      std::cout << (char*)report.getRawStorage() << std::endl;
+      vm.step();
+   }
+   */
+
+   std::cout << "Top: " << vm.regA().describe().c_ize() << std::endl;
+}
+
+};
+
+// This is just a test.
+int main( int argc, char* argv[] )
+{
+   std::cout << "Rule test!" << std::endl;
+
+   TRACE_ON();
+
+   RuleApp app;
+   app.guardAndGo();
+
+   return 0;
+}
+
