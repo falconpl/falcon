91a5f5e237a42cd5fe24512650775e2117a584d0
Paul Davey <plmdvy@gmail.com>
2012-11-07 04:30:45 +1300

    Make the check compatible method of mantra think that synfunctions are compatible with functions.

--- engine/mantra.cpp
+++ engine/mantra.cpp
@@ -10,7 +10,7 @@
    -------------------------------------------------------------------
    (C) Copyright 2012: the FALCON developers (see list in AUTHORS file)
 */
-  
+
 #undef SRC
 #define SRC "engine/mantra.cpp"
 
@@ -18,16 +18,16 @@
 #include <falcon/module.h>
 #include <falcon/engine.h>
 
-namespace Falcon 
+namespace Falcon
 {
-      
+
 Mantra::Mantra():
    m_category( e_c_none ),
    m_module( 0 ),
    m_sr( 0, 0 ),
    m_mark(0)
 {}
-   
+
 Mantra::Mantra( const String& name, int32 line, int32 chr ):
    m_category( e_c_none ),
    m_name( name ),
@@ -42,16 +42,16 @@ Mantra::Mantra( const String& name, Module* module, int32 line, int32 chr ):
    m_module( module ),
    m_sr( line, chr ),
    m_mark(0)
-{}   
- 
+{}
+
 
 Class* Mantra::handler() const
 {
    static Class* cls = Engine::instance()->mantraClass();
-   
+
    return cls;
 }
-  
+
 void Mantra::locateTo( String& target ) const
 {
    if ( m_module == 0 )
@@ -63,14 +63,14 @@ void Mantra::locateTo( String& target ) const
       {
          target = m_module->uri();
       }
-      else {         
+      else {
          target = "[" + m_module->name() + "]";
       }
-      
+
       if( m_sr.line() != 0 )
       {
          target.A("(").N( m_sr.line() );
-         if( m_sr.chr() ) 
+         if( m_sr.chr() )
          {
             target.A(":").N( m_sr.chr() );
          }
@@ -78,17 +78,17 @@ void Mantra::locateTo( String& target ) const
       }
       target += ":";
    }
-   
+
    target += m_name;
 }
-   
-   
+
+
 void Mantra::gcMark( uint32 mark )
 {
    if( mark > m_mark )
    {
       m_mark = mark;
-      
+
       if( m_module != 0 ) {
          m_module->gcMark( mark );
       }
@@ -106,7 +106,7 @@ bool Mantra::isCompatibleWith( Mantra::t_category cat ) const
    // are we searching a class?
    else if( cat == Mantra::e_c_class )
    {
-      if( this->category() == Mantra::e_c_class 
+      if( this->category() == Mantra::e_c_class
           || this->category() == Mantra::e_c_falconclass
           || this->category() == Mantra::e_c_hyperclass
           || this->category() == Mantra::e_c_metaclass )
@@ -117,8 +117,9 @@ bool Mantra::isCompatibleWith( Mantra::t_category cat ) const
    // are we searching a function?
    else if( cat == Mantra::e_c_function )
    {
-      if( this->category() == Mantra::e_c_function 
-          || this->category() == Mantra::e_c_pseudofunction )
+      if( this->category() == Mantra::e_c_function
+          || this->category() == Mantra::e_c_pseudofunction
+          || this->category() == Mantra::e_c_synfunction )
       {
          return true;
       }
@@ -128,7 +129,7 @@ bool Mantra::isCompatibleWith( Mantra::t_category cat ) const
    {
       return true;
    }
-   
+
    return false;
 }
 
