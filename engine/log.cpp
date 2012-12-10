ba8a485d9274d2d05b65d1767272964a75072f27
Giancarlo Niccolai <gc@falconpl.org>
2012-12-10 18:32:42 +0100

    Fixed everything in the barebone of interactive and incremental compiler. Need to clean up the API and sequence with the new relation to Process

---
+++ engine/log.cpp
@@ -0,0 +1,23 @@
+/*
+   FALCON - The Falcon Programming Language.
+   FILE: log.cpp
+
+   Engine-level pluggable asynchronous logging facility.
+   -------------------------------------------------------------------
+   Author: Giancarlo Niccolai
+   Begin: Sat, 08 Dec 2012 22:29:41 +0100
+
+   -------------------------------------------------------------------
+   (C) Copyright 2012: the FALCON developers (see list in AUTHORS file)
+
+   See LICENSE file for licensing details.
+*/
+
+
+   class Message {
+
+   };
+   SyncQueue<Message> m_msgList;
+
+
+/* end of log.cpp */
---
+++ include/falcon/log.h
@@ -0,0 +1,10 @@
+/*
+ * log.h
+ *
+ *  Created on: 08/dic/2012
+ *      Author: gian
+ */
+
+
+
+
