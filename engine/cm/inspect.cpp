8a74580b774a30c8e30cec4a57a927949be1f3a1
Giancarlo Niccolai <gc@falconpl.org>
2013-05-01 20:55:32 +0200

    Added a basic test for dynamic code

--- engine/cm/inspect.cpp
+++ engine/cm/inspect.cpp
@@ -21,6 +21,12 @@
 #include <falcon/vmcontext.h>
 #include <falcon/error.h>
 
+#include <falcon/itemarray.h>
+#include <falcon/itemdict.h>
+#include <falcon/flexydict.h>
+#include <falcon/falconinstance.h>
+#include <falcon/textwriter.h>
+
 namespace Falcon {
 namespace Ext {
 
@@ -37,12 +43,176 @@ Inspect::~Inspect()
 {
 }
 
-void Inspect::invoke( VMContext* , int32  )
+static void internal_inspect( TextWriter* tw, const Item& itm, int32 depth, int32 maxdepth, int32 maxsize )
 {
-   throw paramError();
-   // TODO
+   String temp;
+   Class* cls = 0;
+   void* data = 0;
+
+   tw->write( PStep::renderPrefix(depth ) );
+
+   if( maxdepth > 0 && depth == maxdepth )
+   {
+      tw->write("...");
+      return;
+   }
+
+   // normally, inspect asks the describe function of the class to do the hard work,
+   // but in case of classes, prototypes, arrays and dictionaries, it does a special work.
+   switch( itm.type() )
+   {
+   case FLC_CLASS_ID_ARRAY:
+   {
+      ItemArray* ia = itm.asArray();
+      if( ia->length() == 0 )
+      {
+         tw->write("[]");
+      }
+      else
+      {
+         tw->write("[\n");
+
+         for( length_t len = 0; ia->length(); ++len )
+         {
+            Item& item = ia->at(len);
+            internal_inspect(tw, item, depth+1, maxdepth, maxsize );
+            if( len+1 < ia->length() )
+            {
+               tw->write(",");
+            }
+         }
+
+         tw->write( PStep::renderPrefix(depth ) );
+         tw->write("]");
+      }
+   }
+   break;
+
+   case FLC_CLASS_ID_DICT:
+   {
+      ItemDict* id = itm.asDict();
+      if( id->empty() == 0 )
+      {
+         tw->write("[=>]");
+      }
+      else
+      {
+         tw->write("[\n");
+
+         class Rator: public ItemDict::Enumerator
+         {
+         public:
+            Rator(TextWriter* tw, int32 depth, int32 maxdepth, int32 maxsize ):
+               m_tw(tw), m_depth(depth), m_maxdepth(maxdepth), m_maxsize(maxsize)
+               {}
+            virtual ~Rator() {}
+
+            virtual void operator()( const Item& key, Item& value )
+            {
+               internal_inspect( m_tw, key, m_depth, m_maxdepth, m_maxsize );
+               m_tw->write( " => ");
+               internal_inspect( m_tw, value, m_depth, m_maxdepth, m_maxsize );
+            }
+
+         private:
+            TextWriter* m_tw;
+            int32 m_depth;
+            int32 m_maxdepth;
+            int32 m_maxsize;
+         };
+
+         Rator rator(tw, depth+1, maxdepth, maxsize);
+         id->enumerate(rator);
+         tw->write( PStep::renderPrefix(depth ) );
+         tw->write("]");
+      }
+   }
+   break;
+
+   case FLC_CLASS_ID_PROTO:
+   {
+      FlexyDict* fd = static_cast<FlexyDict*>(itm.asInst());
+
+      if( fd->empty() == 0 )
+      {
+         tw->write("p{}");
+      }
+      else
+      {
+         tw->write("p{\n");
+
+         class Rator: public Class::PVEnumerator
+         {
+         public:
+            Rator(TextWriter* tw, int32 depth, int32 maxdepth, int32 maxsize ):
+               m_tw(tw), m_depth(depth), m_maxdepth(maxdepth), m_maxsize(maxsize)
+               {}
+            virtual ~Rator() {}
+
+            virtual void operator()( const String& property, Item& value )
+            {
+               m_tw->write( property );
+               m_tw->write( " => ");
+               internal_inspect( m_tw, value, m_depth, m_maxdepth, m_maxsize );
+            }
+
+         private:
+            TextWriter* m_tw;
+            int32 m_depth;
+            int32 m_maxdepth;
+            int32 m_maxsize;
+         };
+
+         Rator rator(tw, depth+1, maxdepth, maxsize);
+         fd->enumeratePV(rator);
+         tw->write( PStep::renderPrefix(depth ) );
+         tw->write("}");
+      }
+   }
+   break;
+
+   default:
+      itm.forceClassInst(cls, data);
+      if( cls->isFalconClass() )
+      {
+         /*FalconInstance* fi = static_cast<FalconInstance*>( data );
+         FalconClass* fcls = static_cast<FalconClass*>(cls);
+         fcls->enumerateProperties()
+         */
+      }
+      else {
+         cls->describe(data, temp, depth, maxsize );
+         tw->write( temp );
+      }
+      break;
+   }
+
+   if( depth == 0 )
+   {
+      tw->write("\n");
+   }
 }
 
+void Inspect::invoke( VMContext* ctx, int32 )
+{
+   Item* i_item = ctx->param(0);
+   Item* i_maxdepth = ctx->param(1);
+   Item* i_maxsize = ctx->param(2);
+
+   if( i_item == 0
+            || (i_maxdepth != 0 && !i_maxdepth->isOrdinal())
+            || (i_maxsize != 0 && !i_maxsize->isOrdinal())
+            )
+   {
+      throw paramError();
+   }
+
+   // prepare the local frame
+   int64 maxdepth = i_maxdepth == 0 ? -1 : i_maxdepth->forceInteger();
+   int64 maxsize = i_maxsize == 0 ? -1 : i_maxsize->forceInteger();
+
+   internal_inspect( ctx->vm()->textOut(), *i_item, 0, maxdepth, maxsize );
+}
 
 }
 }
