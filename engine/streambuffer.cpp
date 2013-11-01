36c247eacbde55b90c25e6d301b239016552b181
Giancarlo Niccolai <gc@falconpl.org>
2013-11-01 18:15:06 +0100

    Now works correctly on both ways of a piped stream

--- engine/streambuffer.cpp
+++ engine/streambuffer.cpp
@@ -30,36 +30,62 @@ namespace Falcon {
 StreamBuffer::StreamBuffer( Stream *underlying, uint32 bufSize ):
    m_bufSize( bufSize ),
    m_changed( false ),
-   m_bufPos(0),
-   m_bufLen(0),
+   m_rBufPos(0),
+   m_rBufLen(0),
+   m_wBufPos(0),
+   m_wBufLen(0),
    m_filePos(0),
    m_bReseek( false ),
    m_stream( underlying )
 {
    m_stream->incref();
-   m_buffer = (byte *) malloc( m_bufSize );
+   m_rbuffer = (byte *) malloc( m_bufSize );
+   if( underlying->hasPipeSemantic() )
+   {
+      m_wbuffer = (byte *) malloc( m_bufSize );
+   }
+   else {
+      m_wbuffer = m_rbuffer;
+   }
 }
 
 StreamBuffer::StreamBuffer( const StreamBuffer &other ):
    Stream( other ),
    m_bufSize( other.m_bufSize ),
    m_changed( other.m_changed ),
-   m_bufPos( other.m_bufPos ),
-   m_bufLen( other.m_bufLen ),
+   m_rBufPos( other.m_rBufPos ),
+   m_rBufLen( other.m_rBufLen ),
+   m_wBufPos( other.m_wBufPos ),
+   m_wBufLen( other.m_wBufLen ),
    m_filePos( other.m_filePos ),
    m_bReseek( other.m_bReseek )
 {
    m_stream = other.m_stream;
    m_stream->incref();
-   m_buffer = (byte *) malloc( m_bufSize );
+   m_rbuffer = (byte *) malloc( m_bufSize );
+   memcpy( m_rbuffer, other.m_rbuffer, other.m_rBufLen );
+
+   if( m_stream->hasPipeSemantic() )
+   {
+      m_wbuffer = (byte *) malloc( m_bufSize );
+      memcpy( m_wbuffer, other.m_wbuffer, other.m_wBufLen );
+   }
+   else {
+      m_wbuffer = m_rbuffer;
+   }
 }
 
 
 StreamBuffer::~StreamBuffer()
 {
    flush();
+   bool freeWrite = m_stream->hasPipeSemantic();
    m_stream->decref();
-   free( m_buffer );
+   free( m_rbuffer );
+   if( freeWrite )
+   {
+      free(m_wbuffer);
+   }
 }
 
 
@@ -80,20 +106,32 @@ bool StreamBuffer::refill()
       m_bReseek = false;
    }
    else {
-      m_filePos += m_bufLen;
+      m_filePos += m_rBufLen;
    }
 
-   m_bufPos = 0;
+   m_rBufPos = 0;
+   if( ! m_stream->hasPipeSemantic() )
+   {
+      m_wBufPos = m_rBufPos;
+   }
 
-   int32 readIn = m_stream->read( m_buffer, m_bufSize );
+   int32 readIn = m_stream->read( m_rbuffer, m_bufSize );
    if ( readIn < 0 )
    {
-      m_bufLen = 0;
+      m_rBufLen = 0;
+      if( ! m_stream->hasPipeSemantic() )
+      {
+         m_wBufLen = m_rBufLen;
+      }
       return false;
    }
 
    // changed was set to false from flush
-   m_bufLen = readIn;
+   m_rBufLen = readIn;
+   if( ! m_stream->hasPipeSemantic() )
+   {
+      m_wBufLen = m_rBufLen;
+   }
 
    return true;
 }
@@ -109,7 +147,7 @@ size_t StreamBuffer::read( void *b, size_t size )
 
    byte *buf = (byte*) b;
 
-   size_t avail = m_bufLen - m_bufPos;
+   size_t avail = m_rBufLen - m_rBufPos;
 
    // have we something to store in the buffer?
    if ( avail > 0 )
@@ -117,17 +155,19 @@ size_t StreamBuffer::read( void *b, size_t size )
       // enough to fill the buffer?
       if ( avail >= size )
       {
-         memcpy( buf, m_buffer + m_bufPos, size );
-         m_bufPos += size;
+         memcpy( buf, m_rbuffer + m_rBufPos, size );
+         m_rBufPos += size;
          return size;
       }
       else {
          // in the meanwhile, put the data in.
-         memcpy( buf, m_buffer + m_bufPos, avail );
-         m_bufPos = m_bufLen;  // declare we have consumed everything.
+         memcpy( buf, m_rbuffer + m_rBufPos, avail );
+         m_rBufPos = m_rBufLen;  // declare we have consumed everything.
          // return a partial read in case of underlying networks
          if ( m_stream->hasPipeSemantic() )
+         {
             return avail;
+         }
 
       }
    }
@@ -146,18 +186,25 @@ size_t StreamBuffer::read( void *b, size_t size )
          return m_stream->bad() ? -1 : avail;
       }
 
-      // if the refill operation succed, it is still possible that it has read less than toBeRead.
-      if ( m_bufLen < toBeRead )
-         toBeRead = m_bufLen;
+      // if the refill operation succeed, it is still possible that it has read less than toBeRead.
+      if ( m_rBufLen < toBeRead )
+      {
+         toBeRead = m_rBufLen;
+      }
 
-      memcpy( buf + avail, m_buffer, toBeRead );
-      m_bufPos = toBeRead;  // declare we have consumed the data.
+      memcpy( buf + avail, m_rbuffer, toBeRead );
+      m_rBufPos = toBeRead;  // declare we have consumed the data.
+      if( ! m_stream->hasPipeSemantic() )
+      {
+         m_wBufPos = m_rBufPos;
+         m_wBufLen = m_rBufLen;
+      }
       return toBeRead + avail;
    }
    else
    {
       // it's of no use to refill the buffer now. Just read the required size and update the file pointer.
-      m_filePos += m_bufLen;  // a buffer is gone.
+      m_filePos += m_rBufLen;  // a buffer is gone.
 
       int32 readin = m_stream->read( buf + avail, size - avail );
       if( readin > 0 )
@@ -185,7 +232,7 @@ size_t StreamBuffer::write( const void *b, size_t size )
    const byte *buf = (byte*) b;
 
    // first; is there any space left in the buffer for write?
-   size_t avail = m_bufSize - m_bufPos;
+   size_t avail = m_bufSize - m_wBufPos;
    if( avail > 0 )
    {
       m_changed = true;
@@ -193,19 +240,25 @@ size_t StreamBuffer::write( const void *b, size_t size )
       // good; if we have enough space, advance and go away.
       if ( size <= avail )
       {
-         memcpy( m_buffer + m_bufPos, buf, size );
-         m_bufPos += size;
-         if ( m_bufLen < m_bufPos )
+         memcpy( m_wbuffer + m_wBufPos, buf, size );
+         m_wBufPos += size;
+         if ( m_wBufLen < m_wBufPos )
          {
-            m_bufLen = m_bufPos;
+            m_wBufLen = m_wBufPos;
+         }
+
+         if( ! m_stream->hasPipeSemantic() )
+         {
+            m_rBufPos = m_wBufPos;
+            m_rBufLen = m_wBufLen;
          }
 
          return size;
       }
       else {
          // nay, we can write only part of the stuff.
-         memcpy( m_buffer + m_bufPos, buf, avail );
-         m_bufPos = m_bufLen = m_bufSize;
+         memcpy( m_wbuffer + m_wBufPos, buf, avail );
+         m_wBufPos = m_wBufLen = m_bufSize;
       }
    }
 
@@ -219,8 +272,12 @@ size_t StreamBuffer::write( const void *b, size_t size )
       flush();
       m_changed = true; // ensure we declare this buffer changed anyhow
 
-      memcpy( m_buffer, buf + avail, toBeWritten );
-      m_bufPos = m_bufLen = toBeWritten;
+      memcpy( m_wbuffer, buf + avail, toBeWritten );
+      m_wBufPos = m_wBufLen = toBeWritten;
+      if( ! m_stream->hasPipeSemantic() )
+      {
+         m_rBufPos = m_rBufLen = toBeWritten;
+      }
 
       return avail + toBeWritten;
    }
@@ -247,7 +304,7 @@ bool StreamBuffer::close()
 
 int64 StreamBuffer::tell()
 {
-   return m_filePos + m_bufPos;
+   return m_filePos + m_rBufPos;
 }
 
 bool StreamBuffer::truncate( int64 pos )
@@ -255,9 +312,9 @@ bool StreamBuffer::truncate( int64 pos )
    if ( pos == -1 )
    {
       // shorten the buffer to the current position
-      m_bufLen = m_bufPos;
+      m_rBufLen = m_rBufPos;
       // And truncate
-      if(  m_stream->truncate( m_filePos + m_bufPos ) )
+      if(  m_stream->truncate( m_filePos + m_rBufPos ) )
       {
          flush();
          return true;
@@ -267,7 +324,7 @@ bool StreamBuffer::truncate( int64 pos )
    }
    else
    {
-      int64 curpos = m_filePos + m_bufPos;
+      int64 curpos = m_filePos + m_rBufPos;
       flush();
 
       // and trunk at the desired position
@@ -287,12 +344,12 @@ int64 StreamBuffer::seek( int64 pos, e_whence whence )
    // TODO: optimize and avoid re-buffering if we're still in the buffer.
    if( whence == ew_cur )
    {
-      pos = m_filePos + m_bufPos + pos;
+      pos = m_filePos + m_rBufPos + pos;
       whence = ew_begin;
    }
 
    flush();
-   m_bufLen = m_bufPos = 0;
+   m_rBufLen = m_rBufPos = 0;
 
    m_filePos = m_stream->seek( pos, whence );
    m_bReseek = false;
@@ -301,24 +358,28 @@ int64 StreamBuffer::seek( int64 pos, e_whence whence )
 
 bool StreamBuffer::flush()
 {
-   if ( ! m_changed || m_bufLen == 0 )
+   if ( ! m_changed || m_wBufLen == 0 )
       return true;
 
-   int32 written = m_stream->write( m_buffer, m_bufLen );
+   int32 written = m_stream->write( m_wbuffer, m_wBufLen );
    uint32 count = 0;
-   while( written > 0 && count < m_bufLen )
+   while( written > 0 && count < m_wBufLen )
    {
       count += (uint32) written;
-      written = m_stream->write( m_buffer + count, m_bufLen - count );
+      written = m_stream->write( m_wbuffer + count, m_wBufLen - count );
    }
 
    if( written < 0 )
       return false;
 
-   m_filePos += m_bufPos;
-   m_bReseek = m_bReseek || m_bufPos != m_bufLen;
+   m_filePos += m_wBufPos;
+   m_bReseek = m_bReseek || m_wBufPos != m_wBufLen;
    m_changed = false;
-   m_bufPos = m_bufLen = 0;
+   m_wBufPos = m_wBufLen = 0;
+   if( ! m_stream->hasPipeSemantic() )
+   {
+      m_rBufPos = m_rBufLen = 0;
+   }
 
    m_stream->flush();
 
@@ -328,37 +389,37 @@ bool StreamBuffer::flush()
 
 bool StreamBuffer::get( uint32 &chr )
 {
-   if ( m_bufPos == m_bufLen )
+   if ( m_rBufPos == m_rBufLen )
    {
       if ( ! refill() )
          return false;
 
-      if( m_bufPos == m_bufLen ) // eof?
+      if( m_rBufPos == m_rBufLen ) // eof?
          return false;
    }
 
-   chr = m_buffer[m_bufPos++ ];
+   chr = m_rbuffer[m_rBufPos++ ];
    return true;
 }
 
 
 bool StreamBuffer::put( uint32 chr )
 {
-   if ( m_bufPos == m_bufSize )
+   if ( m_wBufPos == m_bufSize )
    {
       if ( ! flush() )
          return false;
 
-      m_buffer[ 0 ] = chr;
-      m_bufPos = m_bufLen = 1;
+      m_wbuffer[ 0 ] = chr;
+      m_wBufPos = m_wBufLen = 1;
       m_changed = true;
       return true;
    }
 
-   m_buffer[ m_bufPos++ ] = chr;
+   m_wbuffer[ m_rBufPos++ ] = chr;
    m_changed = true;
-   if ( m_bufLen < m_bufPos )
-      m_bufLen = m_bufPos;
+   if ( m_wBufLen < m_wBufPos )
+      m_wBufLen = m_wBufPos;
    return true;
 }
 
@@ -369,11 +430,23 @@ bool StreamBuffer::resizeBuffer( uint32 size )
    if(size != m_bufSize )
    {
       if ( ! flush() )
+      {
          return false;
+      }
 
-      free( m_buffer );
-      m_buffer = (byte*) malloc( size );
-      m_bufSize = size;  
+      free( m_rbuffer );
+      m_rbuffer = (byte*) malloc( size );
+      m_bufSize = size;
+      m_rBufLen = m_rBufPos = 0;
+
+      if( m_stream->hasPipeSemantic() )
+      {
+         free(m_wbuffer);
+         m_wbuffer = (byte*) malloc( size );
+      }
+      else {
+         m_wbuffer = m_rbuffer;
+      }
    }
    
    return true;
