c9fa4741a5a61f72cf6d3372ce974ec1e9d9c87b
Giancarlo Niccolai <gc@falconpl.org>
2013-11-22 18:34:17 +0100

    Session class ready but for complete storage

--- modules/native/feathers/shmem/session.cpp
+++ modules/native/feathers/shmem/session.cpp
@@ -22,7 +22,11 @@
 #include <falcon/error.h>
 #include <falcon/stderrors.h>
 #include <falcon/stdhandlers.h>
-#include <falcon/.h>
+#include <falcon/storer.h>
+#include <falcon/restorer.h>
+#include <falcon/itemid.h>
+
+#include <falcon/sys.h>
 
 #include <map>
 
@@ -35,21 +39,36 @@ public:
 
    Mutex m_mtxSym;
    SymbolSet m_symbols;
+   uint32 m_version;
 
-   Private() {}
+   Private():
+      m_version(0)
+   {}
+
    ~Private() {}
 };
+
 
 Session::Session()
 {
    _p = new Private;
+   m_timeout = 0;
+   m_tsCreation = 0;
+   m_tsExpire = 0;
+   m_bExpired = false;
 }
 
-Session::Session( const String& name ):
-         m_name(name)
+
+Session::Session( const String& name, int64 to ):
+   m_id(name)
 {
    _p = new Private;
+   m_timeout = 0;
+   m_tsCreation = to;
+   m_tsExpire = 0;
+   m_bExpired = false;
 }
+
 
 Session::~Session()
 {
@@ -57,14 +76,54 @@ Session::~Session()
 }
 
 
+void Session::getID( String& target ) const
+{
+   _p->m_mtxSym.lock();
+   target = m_id;
+   _p->m_mtxSym.unlock();
+}
+
+
+void Session::setID( const String& target )
+{
+   _p->m_mtxSym.lock();
+   m_id = target;
+   _p->m_mtxSym.unlock();
+}
+
+
+void Session::start()
+{
+   int64 now = Sys::_epoch();
+
+   _p->m_mtxSym.lock();
+   if( ! m_bExpired )
+   {
+      m_tsCreation = now;
+      if( m_timeout != 0 )
+      {
+         m_tsExpire = now + m_tsCreation;
+      }
+      else {
+         m_tsExpire = 0;
+      }
+   }
+   _p->m_mtxSym.unlock();
+}
+
+
 void Session::addSymbol(Symbol* sym, const Item& value)
 {
    _p->m_mtxSym.lock();
    Private::SymbolSet::iterator iter = _p->m_symbols.find(sym);
+   _p->m_version++;
    if( iter != _p->m_symbols.end() )
    {
       sym->incref();
       _p->m_symbols.insert(std::make_pair(sym, value));
+   }
+   else {
+      iter->second = value;
    }
    _p->m_mtxSym.unlock();
 }
@@ -76,6 +135,7 @@ bool Session::removeSymbol(Symbol* sym)
    Private::SymbolSet::iterator iter = _p->m_symbols.find(sym);
    if( iter != _p->m_symbols.end() )
    {
+      _p->m_version++;
       _p->m_symbols.erase(iter);
       _p->m_mtxSym.unlock();
 
@@ -89,9 +149,11 @@ bool Session::removeSymbol(Symbol* sym)
    return false;
 }
 
+
 void Session::record( VMContext* ctx )
 {
    _p->m_mtxSym.lock();
+   _p->m_version++;
    Private::SymbolSet::iterator iter = _p->m_symbols.begin();
    while( iter != _p->m_symbols.begin() )
    {
@@ -158,7 +220,12 @@ void Session::restore(Restorer* restorer)
    bool first = false;
    while( restorer->next(handler,data,first) )
    {
-      if( handler->typeID() == FLC_T)
+      if( handler->typeID() == FLC_ITEM_NIL )
+      {
+         // we're done
+         return;
+      }
+
       // the first should be a symbol
       if( handler != symClass )
       {
@@ -175,6 +242,150 @@ void Session::restore(Restorer* restorer)
       value.deuser();
       addSymbol( sym, value );
    }
+}
+
+
+bool Session::get(Symbol* sym, Item& item) const
+{
+   bool res = false;
+   _p->m_mtxSym.lock();
+   Private::SymbolSet::iterator iter = _p->m_symbols.find(sym);
+   if( iter != _p->m_symbols.end() )
+   {
+      item = iter->second;
+      res = true;
+   }
+   _p->m_mtxSym.unlock();
+
+   return res;
+}
+
+
+bool Session::get(const String& symName, Item& item) const
+{
+   Symbol* sym = Engine::getSymbol(symName);
+   bool res = get(sym, item);
+   sym->decref();
+   return res;
+}
+
+
+int64 Session::createdAt() const
+{
+   _p->m_mtxSym.lock();
+   int64 c = m_tsCreation;
+   _p->m_mtxSym.unlock();
+   return c;
+}
+
+
+int64 Session::expiresAt() const
+{
+   _p->m_mtxSym.lock();
+   int64 c = m_tsExpire;
+   _p->m_mtxSym.unlock();
+   return c;
+}
+
+
+int64 Session::timeout() const
+{
+   _p->m_mtxSym.lock();
+   int64 c = m_timeout;
+   _p->m_mtxSym.unlock();
+   return c;
+}
+
+
+void Session::timeout( int64 to )
+{
+   int64 timeNow = 0;
+   if( to > 0 )
+   {
+      timeNow = Sys::_epoch();
+
+      _p->m_mtxSym.lock();
+      m_timeout = to;
+      m_tsExpire = timeNow + to;
+      _p->m_mtxSym.unlock();
+   }
+   else {
+      _p->m_mtxSym.lock();
+      m_timeout = 0;
+      m_tsExpire = 0;
+      _p->m_mtxSym.unlock();
+   }
+}
+
+
+void Session::tick()
+{
+   int64 timeNow = Sys::_epoch();
+
+   _p->m_mtxSym.lock();
+   if( m_timeout > 0 )
+   {
+      m_tsExpire = timeNow + m_timeout;
+   }
+   _p->m_mtxSym.unlock();
+}
+
+
+bool Session::isExpired() const
+{
+   // optimistic check on a locked variable.
+   // expired can change only from false to true.
+   if( m_bExpired )
+   {
+      return true;
+   }
+
+   int64 timeNow = Sys::_epoch();
+   bool bExpired = false;
+
+   _p->m_mtxSym.lock();
+   if( m_tsExpire > 0 && timeNow > m_tsExpire )
+   {
+      bExpired = true;
+      m_bExpired = true;
+   }
+   _p->m_mtxSym.unlock();
+
+   return bExpired;
+}
+
+
+void Session::gcMark( uint32 mark )
+{
+   if( m_mark == mark )
+   {
+      return;
+   }
+
+   m_mark = mark;
+
+   _p->m_mtxSym.lock();
+   uint32 version = _p->m_version;
+   Private::SymbolSet::iterator iter = _p->m_symbols.begin();
+   while( iter != _p->m_symbols.end() )
+   {
+      Item item = iter->second;
+      _p->m_mtxSym.unlock();
+
+      item.gcMark(mark);
+
+      // mark again from the start if the map changed.
+      _p->m_mtxSym.lock();
+      if( version == _p->m_version )
+      {
+         ++iter;
+      }
+      else {
+         iter = _p->m_symbols.begin();
+         version = _p->m_version;
+      }
+   }
+   _p->m_mtxSym.unlock();
 }
 
 }
--- modules/native/feathers/shmem/session.h
+++ modules/native/feathers/shmem/session.h
@@ -18,27 +18,73 @@
 
 #include <falcon/setup.h>
 #include <falcon/string.h>
+#include <falcon/item.h>
 
 namespace Falcon
 {
 
 class Symbol;
 class VMContext;
+class Storer;
+class Restorer;
 
+/** Automatism to implement persistent data across multiple runs of a program.
+ *
+ */
 class Session
 {
 public:
+   /** Creates an empty session.
+    *
+    * When created in this form, the session won't be given any ID
+    * nor expiration time.
+    *
+    * ID and expiration time must be provided separately before the
+    * session can be stored.
+    */
    Session();
-   Session( const String& name );
+
+   /** Creates a session, and possibly set an expiration time.
+    *
+    * This constructor creates a session and possibly immediately assigns
+    * an expiration time to it.
+    *
+    * Notice that it's preferably to let the session start to expire as soon
+    * as the session is first saved on a storage.
+    *
+    */
+   Session( const String& id, int64 to = 0 );
+
+   /** Destroys the session */
    ~Session();
 
-   const String& name() const { return m_name; }
-   void name( const String& n ) { m_name = n; }
+   /** Starts the session as a new session right now. */
+   void start();
 
+   /** Gets the ID of the session.
+    * \param target the string where the ID of the session is stored.
+    *
+    * \note The method is threadsafe (this is why you have to provide
+    * a target string where to place the ID).
+    */
+   void getID( String& target ) const;
+
+   /** Sets an ID for the session.
+    *
+    * \note The method is threadsafe.
+    */
+   void setID( const String& n );
+
+   /** Adds a symbol (with its value) to the session recording. */
    void addSymbol(Symbol* sym, const Item& value=Item());
+
+   /** Removes a symbol from the session recording */
    bool removeSymbol(Symbol* sym);
 
+   /** Records the required symbols from the given context. */
    void record(VMContext* ctx);
+
+   /** Updates the given context with the values stored in the session. */
    void apply(VMContext* ctx) const;
 
    /**
@@ -53,15 +99,61 @@ public:
    void store(VMContext* ctx, Storer* storer) const;
 
    /**
-    * Restores the session from an alaredy loaded restorer.
+    * Restores the session from an already loaded restorer.
     *
     * The given restorer must have been already loaded via
     * Restorer::restore.
     */
    void restore(Restorer* restorer);
 
+   /** Gets the value stored in the session for a symbol. */
+   bool get(Symbol* sym, Item& value) const;
+   /** Gets the value stored in the session for a symbol. */
+   bool get(const String& symName, Item& value) const;
+
+   /** Returns the timestamp when the start() method was called. */
+   int64 createdAt() const;
+   /** Returns the timestamp when the session expires */
+   int64 expiresAt() const;
+
+   /** Returns the timeout set for the session.
+    *
+    * The timeout is the amount of time that is added to the expiration
+    * time each time the session is refreshed.
+    */
+   int64 timeout() const;
+
+   /** Changes the timeout (and refreshes the session).
+    *
+    * If the timeout is set to zero, the session never expires.
+    */
+   void timeout( int64 to );
+
+   /** Extends the session lifetime of the given timeout.
+    *
+    * If the session has a timeout, the expiration time is set
+    * to the current moment in time plus the given timeout; otherwise
+    * this call has no effect.
+    *
+    */
+   void tick();
+
+   /** Checks if a session is currently expired. */
+   bool isExpired() const;
+
+   void gcMark( uint32 mark );
+   uint32 currentMark() const { return m_mark; }
+
 private:
-   String m_name;
+   /** Not cloneable */
+   Session(const Session& ) {}
+
+   String m_id;
+   int64 m_tsCreation;
+   int64 m_tsExpire;
+   int64 m_timeout;
+   mutable bool m_bExpired;
+   uint32 m_mark;
 
    class Private;
    Private* _p;
