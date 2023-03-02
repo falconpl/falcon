9cc8894d8ab6033f2241f5804f4fc6868be47719
Stanislas Marquis <stan@astrorigin.com>
2023-03-02 11:18:17 +0100

    Fix cmake module so that it can find readline.
    Also made the module more readable.

--- cmake/modules/FindEditline.cmake
+++ cmake/modules/FindEditline.cmake
@@ -1,24 +1,32 @@
-option(FALCON_WITH_GPL_READLINE "" off)
-if(FALCON_WITH_GPL_READLINE)
-  set(_header readline/readline.h)
-  set(_libname readline)
+### FindEditLine.cmake module for Falcon
+
+option( FALCON_WITH_GPL_READLINE "Insist on using GNU readline" OFF )
+
+# unset these so that we can actually find GNU readline
+unset( Editline_INCLUE_DIR CACHE )
+unset( Editline_LIBRARY CACHE )
+
+if( FALCON_WITH_GPL_READLINE )
+  set( _header readline/readline.h )
+  set( _libname readline )
 else()
-  set(_header editline/readline.h)
-  set(_libname edit)
+  set( _header editline/readline.h )
+  set( _libname edit )
 endif()
 
-find_path(Editline_INCLUE_DIR ${_header})
-find_library(Editline_LIBRARY ${_libname})
+find_path( Editline_INCLUE_DIR ${_header} )
+find_library( Editline_LIBRARY ${_libname} )
 
-include(CheckSymbolExists)
-set(CMAKE_REQUIRED_INCLUDES ${Editline_INCLUE_DIR})
-set(CMAKE_REQUIRED_LIBRARIES ${Editline_LIBRARY})
-check_symbol_exists(rl_getc_function "stdio.h;${_header}" 
-  HAVE_RL_GETC_FUNCTION 
-)
+include( CheckSymbolExists )
+set( CMAKE_REQUIRED_INCLUDES ${Editline_INCLUE_DIR} )
+set( CMAKE_REQUIRED_LIBRARIES ${Editline_LIBRARY} )
+check_symbol_exists( rl_getc_function "${_header}" HAVE_RL_GETC_FUNCTION )
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Editline DEFAULT_MSG Editline_LIBRARY Editline_INCLUE_DIR HAVE_RL_GETC_FUNCTION)
+include( FindPackageHandleStandardArgs )
+find_package_handle_standard_args( Editline DEFAULT_MSG
+  Editline_LIBRARY Editline_INCLUE_DIR HAVE_RL_GETC_FUNCTION )
 
-set(Editline_INCLUDE_DIRS ${Editline_INCLUE_DIR})
-set(Editline_LIBRARIES ${Editline_LIBRARY})
+set( Editline_INCLUDE_DIRS ${Editline_INCLUE_DIR} )
+set( Editline_LIBRARIES ${Editline_LIBRARY} )
+
+# vi: ts=2 sw=2 et
